36394c22b01c65bb87883fa066d8a9350f3ec47e
hharshhhhh <117569955+hharshhhhh@users.noreply.github.com>
2023-01-07 23:30:14 +0530

    Add files via upload

---
+++ Email_Checking.py
@@ -0,0 +1,29 @@
+email=input("ENTER YOUR EMAIL ID")
+a,b,c=0,0,0
+if len(email)>=6:
+    if email[0].isalpha():
+        if ("@" in email) and (email.count("@"==1)):
+            if (email[-4]==".")^ (email[-3]=="."):
+                for i in email:
+                    if i==i.isspace():
+                        a=1
+                    elif i.isalpha():
+                        if i==i.upper():
+                            b=1
+                    elif i.isdigit():
+                        continue
+                    elif i=="_" or i=="." or i=="@":
+                        continue
+                    else:
+                        c=1
+
+                if a==1 or b==1 or c==1:
+                    print("Something is wrong")
+            else:
+                print("Error...")
+        else:
+            print("Try Again")
+    else:
+        print("Somethinf=g is Wrong")
+else:
+    print("Wrong Input")
